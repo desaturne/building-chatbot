6d10d18bc244df31d90413d1d05475ff739260ec
Sushma Shrestha <153975721+desaturne@users.noreply.github.com>
2025-05-13 11:57:58 +0545

    final commit

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
-import google.genai as gen_ai
+import google.generativeai as gen_ai
 
 #loading env variables
 load_dotenv()
 
 #configuring streamlit
 st.set_page_config(
-    page_title="Chat with Google GenAI",
+    page_title="Chat with Google GenAI powered Chatbot",
     page_icon=":robot:",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-Google_API_KEY = os.getenv("GOOGLE_API_KEY")
+#setting up google generative ai api key
+Google_API_KEY = os.getenv("Google_API_key")
 
 #setting up model
 gen_ai.configure(api_key=Google_API_KEY)
-model = gen_ai.GenarativeModel('gemini-2.0-flash')
+model = gen_ai.GenerativeModel('gemini-1.5-flash')
 
 #function to translate roles between streamlit and google genai
 def translate_role(role):
@@ -32,8 +33,8 @@ def translate_role(role):
 if "chat_session" not in st.session_state:
     st.session_state.chat_session = model.start_chat(history=[])
 
-#displaying chatbots title
-st.title("Chat with Google GenAI")
+#displaying chatbot's title
+st.title("🤓 Chat With Me.. Lesgoooo")
 
 #displaying chat history
 for message in st.session_state.chat_session.history:
@@ -41,18 +42,15 @@ for message in st.session_state.chat_session.history:
         st.markdown(message.parts[0].text)
 
 #input field for user message
-user_prompt = st.chat_input("Ask you question...")
+user_prompt = st.chat_input("Ask me anything...")
 if user_prompt:
     #displaying user message
     with st.chat_message("user"):
         st.markdown(user_prompt)
-    
-    #sending user message to google genai
-    st.session_state.chat_session.send_message(user_prompt)
-    
+
     #getting response from google genai
-    response = st.session_state.chat_session.get_response()
+    response = st.session_state.chat_session.send_message(user_prompt)
     
     #displaying google genai response
-    with st.chat_message("model"):
-        st.markdown(response.parts[0].text)
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
